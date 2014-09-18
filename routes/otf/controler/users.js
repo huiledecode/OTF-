174ebd6d376ce6f0f4c0c83fb9e693ac0eab74d7
stephane Mascaron <stephane.mascaron@huiledecode.org>
2014-09-19 01:05:53 +0200

    Construction de la requête MongoDB en fonction des paramètres reçu dans l'action du Bean via la requête HTTP.

--- routes/otf/controler/users.js
+++ routes/otf/controler/users.js
@@ -13,6 +13,7 @@ var Accounts = mongoose.model('Accounts');
 
 exports.users = {
     list: function (params, cb) {
+        logger.debug('params : ' , params);
         Accounts.find({}, function (err, list_users) {
             logger.debug('liste des utilisateurs :', list_users);
             return cb(null, {result: list_users});
@@ -20,7 +21,12 @@ exports.users = {
     },
 
     one: function (params, cb) {
-        Accounts.find({login: params}, function (err, one_user) {
+        logger.debug('params : ' , params);
+        /* TODO -> ici il faudra parser les params de façon plus générique pour les requêtes MongoDB
+         * j'ai un plantage lorsque dans les paramètres de la requête Mongo je met :
+         * Accounts.find({params['params_names'][0] :params['params_values'][0]}, function ... */
+        //var login = params['params_names'][0];
+        Accounts.find({login : params['params_values'][0]}, function (err, one_user) {
             logger.debug('Utilisateur sélectionné : ', one_user);
             return cb(null, {result: one_user});
         });
