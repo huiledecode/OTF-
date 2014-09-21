b7b181b1ac2ea42e129b61e436480f8465fe242b
stephane Mascaron <stephane.mascaron@huiledecode.org>
2014-09-21 02:16:31 +0200

    Simplification du passage des paramètres par l'objets "params" qui a été construit dans otf.js suite à la requête HTTP et la comparaisons des champs de params_names" de l'annuaire json.

--- routes/otf/controler/users.js
+++ routes/otf/controler/users.js
@@ -22,15 +22,15 @@ exports.users = {
 
     one: function (params, cb) {
         logger.debug('params : ' , params);
-        /* TODO -> ici il faudra parser les params de façon plus générique pour les requêtes MongoDB
-         * j'ai un plantage lorsque dans les paramètres de la requête Mongo je met :
-         * Accounts.find({params['params_names'][0] :params['params_values'][0]}, function ... */
-        //var login = params['params_names'][0];
-        Accounts.find({login : params['params_values'][0]}, function (err, one_user) {
+        Accounts.find(params, function (err, one_user) {
             logger.debug('Utilisateur sélectionné : ', one_user);
             return cb(null, {result: one_user});
         });
     }
+
+    /* Todo : Réfléchir à l'insertion dans MongoDB de façon aussi générique que le find.
+       Todo : voir du côté des schémas mongoose qui permette 'insérer directement un bodyContents ??
+     */
 };
 
 //xports.users = {
