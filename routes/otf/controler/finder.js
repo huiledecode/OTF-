f11cba8496b580ec0e4951d1bad7cb219f542cf4
stephane Mascaron <stephane.mascaron@huiledecode.org>
2014-10-17 03:53:33 +0200

    réécriture de la méthode "list" et "one" avec appel du module générique mongoose pour éviter la réplication des modèles dans les sources. Ainsi que l'utilisation d'un annuaire_schemas.json pour paramétrer les schémas d'accès à la base mongodb

--- routes/otf/controler/finder.js
+++ routes/otf/controler/finder.js
@@ -6,42 +6,72 @@
  */
 var logger = require('log4js').getLogger('css');
 var mongoose = require('mongoose');
+var genericModel = require(__dirname +'/../../../ressources/models/mongooseGeneric');
 
 /*
  * GET users listing.
  */
 
 exports.finder = {
-    list: function (params, model, room, cb) {
-        logger.debug('params : ', params);
-        var modele = mongoose.model(model);
-        // Test emit WebSocket Event
-        logger.debug(" One User emmit call");
-        sio.sockets.in(room).emit('user', {room: room, comment: ' List of Users\n\t Your Filter is : *'});
-        modele.find({}, function (err, list_users) {
-            logger.debug('liste des utilisateurs :', list_users);
-            return cb(null, {result: list_users, room: room});
-        });
-    },
-
-    one: function (params, model, room, cb) {
-        //@TODO not safety
-        logger.debug('params : ', params['login'].source);
-        logger.debug('room   : ', room);
-        logger.debug('model  : ' + model);
-        //-- Accounts Model
-        var modele = mongoose.model(model);
-        // Test Emit WebSocket Event
-        logger.debug(" One User emmit call");
-        sio.sockets.in(room).emit('user', {room: room, comment: ' One User\n\t Your Filter is :' + params['login'].source});
-        modele.find(params, function (err, one_user) {
-            logger.debug('Utilisateur sélectionné : ', one_user);
-            return cb(null, {result: one_user, room: room});
-        });
-
+  list: function (params, path, model, schema, room, cb) {
+    var document;
+    logger.debug('params : ', params);
+    logger.debug('schema : ', schema);
+    //var modele = mongoose.model(model);
+    logger.debug('global.annuaire_schema avant : ', global.annuaire_schema);
+    // On passe à mongooseGeneric le path unique pour l'action,
+    // comme identifiant de modèle si il n'est pas déjà compilé
+    // dans mongoose
+    // Test emit WebSocket Event
+    logger.debug(" One User emmit call");
+    sio.sockets.in(room).emit('user', {room: room, comment: ' List of Users\n\t Your Filter is : *'});
+    try {
+      document = new genericModel.mongooseGeneric(path, schema, model);
+      document.getDocuments({}, function (err, list_users) {
+        logger.debug('liste des utilisateurs :', list_users);
+        return cb(null, {result: list_users, room: room});
+      });
+    } catch (err) { // si existe pas alors exception et on l'intègre via mongooseGeneric
+      modele = global.db.model(path);
+      // requete ici si model existe dejà dans mongoose
+      modele.find(params, function (err, list_users) {
+        logger.debug('Utilisateur sélectionné : ', list_users);
+        return cb(null, {result: list_users, room: room});
+      });
     }
+  },
 
-    /* Todo : Réfléchir à l'insertion dans MongoDB de façon aussi générique que le find.
-     Todo : voir du côté des schémas mongoose qui permette 'insérer directement un bodyContents ??
-     */
+  one: function (params, path, model, schema, room, cb) {
+      //@TODO not safety
+      logger.debug('params : ', params['login'].source);
+      logger.debug('room   : ', room);
+      logger.debug('model  : ' + model);
+      //-- Accounts Model
+      //var modele = mongoose.model(model);
+    logger.debug('global.annuaire_schema avant : ', global.annuaire_schema);
+    // On passe à mongooseGeneric le path unique pour l'action,
+    // comme identifiant de modèle si il n'est pas déjà compilé
+    // dans mongoose
+    // Test emit WebSocket Event
+    logger.debug(" One User emmit call");
+    sio.sockets.in(room).emit('user', {room: room, comment: ' List of Users\n\t Your Filter is : *'});
+    try {
+      document = new genericModel.mongooseGeneric(path, schema, model);
+      document.getDocuments(params, function (err, one_user) {
+        logger.debug('liste des utilisateurs :', one_user);
+        return cb(null, {result: one_user, room: room});
+      });
+    } catch (err) { // si existe pas alors exception et on l'intègre via mongooseGeneric
+      modele = global.db.model(path);
+      // requete ici si model existe dejà dans mongoose
+      modele.find(params, function (err, one_user) {
+        logger.debug('Utilisateur sélectionné : ', one_user);
+        return cb(null, {result: one_user, room: room});
+      });
+    }
+  }
+
+  /* Todo : Réfléchir à l'insertion dans MongoDB de façon aussi générique que le find.
+   Todo : voir du côté des schémas mongoose qui permette 'insérer directement un bodyContents ??
+   */
 };
