2068624e18ac39e3b604dadb089d8561ec6692ea
stephane Mascaron <stephane.mascaron@huiledecode.org>
2014-10-17 03:54:25 +0200

    ajout des deux paramètres path et schema pour utilisation de la classe mongooseGeneric.js

--- routes/otf/controler/index.js
+++ routes/otf/controler/index.js
@@ -3,7 +3,7 @@
  */
 exports.index = {
 
-    titre: function (params, modele, room, cb) {
+    titre: function (params, path, model, schema, room, cb) {
         return cb(null, { title: 'OTF with Express' });
     }
 
