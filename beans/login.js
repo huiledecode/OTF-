cfc3b2a344a57f67e8b60b169090871bc88869d5
stephane Mascaron <stephane.mascaron@huiledecode.org>
2014-11-29 14:11:06 +0100

    modification des params de la callback avant identification pour éviter Error undefined state, flag, user, message non initialisé avant signupAccount

--- beans/login.js
+++ beans/login.js
@@ -3,7 +3,7 @@ exports.login = {
     titre: function (req, cb) {
         var _controler = req.session.controler;
         //return cb(null, { title: 'OTF with Express Passport Authentification' });
-        return cb(null, { title: 'OTF with ExpressPassport Authentification', "state": req.session.login_info.state, "flag": req.session.login_info.flag, "user": req.session.login_info.user, "message": req.session.login_info.message });
+        return cb(null, { title: 'OTF with ExpressPassport Authentification', "state": "not login", "flag": "none", "user": "none", "message": "identify yourself" });
 
     }
 
