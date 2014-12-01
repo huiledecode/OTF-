61f474707a1034df8fede1ddb65fdaf2b8b1638e
epa <e.papet@dev1-0.com>
2014-12-01 23:13:18 +0100

    Mise à jours du bean login.js gestion des message erreur su login et password failed

--- beans/login.js
+++ beans/login.js
@@ -3,7 +3,20 @@ exports.login = {
     titre: function (req, cb) {
         var _controler = req.session.controler;
         //return cb(null, { title: 'OTF with Express Passport Authentification' });
-        return cb(null, { title: 'OTF with ExpressPassport Authentification', "state": "not login", "flag": "none", "user": "none", "message": "identify yourself" });
+        var data = {};
+        data.title = 'OTF with ExpressPassport Authentification';
+        //
+        if (req.session.login_info) {
+            data.state = req.session.login_info.state;
+            data.user = req.session.login_info.user;
+            data.message = req.session.login_info.message;
+        } else {
+            data.state = null;
+            data.user = null;
+            data.message = null;
+        }
+
+        return cb(null, data);
 
     }
 
