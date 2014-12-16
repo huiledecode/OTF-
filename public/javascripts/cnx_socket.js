0005989df95ba3ff5f3f0222181cca1a40d10025
stephane Mascaron <stephane.mascaron@huiledecode.org>
2014-12-16 16:55:35 +0100

    Ajout d'un écouteur d'evt websocket pour les push serveur via oplog de données insérées

--- public/javascripts/cnx_socket.js
+++ public/javascripts/cnx_socket.js
@@ -3,6 +3,19 @@ socket.on('ack', function (data) {
     console.log('WebSocket Client is connected \n Room is :' + data.sessionid + '\n User is :' + data.user);
 });
 
+socket.on('insertOk', function(data) {
+  console.log('données en provenance du serveur : ' , data);
+  delete data.o.__proto;
+  delete data.o.__v;
+  var objectStruct = Object.getOwnPropertyNames(data.o).sort();
+  console.log('objectStruct : ' , objectStruct);
+  var tabDataToAdd = [];
+  for (var k=0; k < objectStruct.length; k++) {
+    tabDataToAdd.push(data.o[objectStruct[k]]);
+  }
+  $('#listereponses').dataTable().fnAddData(tabDataToAdd);
+});
+
 
 /**
  * Created by epa on 12/09/14.
