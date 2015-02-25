0c6596c9988620e17d332f66bd0e05b6b28506c1
epa <e.papet@dev1-0.com>
2015-02-25 22:29:50 +0100

    Gestion des session par Redis et implémentation du RedisStore avec methode "all" (tableaux de session [sid][data] npm install hiredis redis Switch entre MemoryStore et RediStore par la variable environement SESSION="REDIS" ex

--- ressources/otf_session.js
+++ ressources/otf_session.js
@@ -1,12 +1,23 @@
 /**
  * Created by epa on 10/12/14.
  */
+
+
 module.exports = function (app, secret, cookie_name) {
     var cookieParser = require('cookie-parser');
     var session = require("express-session");
-    // Cookies Managment
-    var MemoryStore = session.MemoryStore;
-    var sessionStore = new MemoryStore();
+    var sessionStore;
+    // Store Memory or Redis
+    if (process.env.NODE_SESSION == "REDIS") {
+        var RedisStore = require('./connect-redis')(session);
+        sessionStore = new RedisStore();
+        console.log("NODE_SESSION = REDIS");
+    } else {
+        var MemoryStore = session.MemoryStore;
+        sessionStore = new MemoryStore();
+        console.log("NODE_SESSION = MEMORY");
+    }
+    // -- Cookies Parser
     app.use(cookieParser(secret));
 //--
     app.use(session({
