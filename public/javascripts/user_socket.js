8a068cbd12798273cfb07cf44c412e9129eeef8e
epa <e.papet@dev1-0.com>
2015-03-02 17:11:06 +0100

    Gestion des session par Redis et implémentation du RedisStore avec methode "all" (tableaux de session [sid][data] npm install hiredis redis Switch entre MemoryStore et RediStore par la variable environement SESSION="REDIS" ex

--- public/javascripts/user_socket.js
+++ public/javascripts/user_socket.js
@@ -1,8 +1,13 @@
-socket.on('user', function (message) {
-    //alert(' WS user event \n Room is :' + message.room + '\n' + message.comment);
-    console.log(' WS user event \n Room is :' + message.room + '\n' + message.comment);
-});
-
 /**
  * Created by epa on 12/09/14.
  */
+
+
+/**
+ *
+ */
+socket.on('user', function (message) {
+    //alert(' WS user event \n Room is :' + message.room + '\n' + message.comment);
+    console.log(' WS Server event \n Room is :' + message.room + '\n' + message.comment);
+});
+
