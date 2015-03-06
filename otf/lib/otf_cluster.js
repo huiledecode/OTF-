cde92c1a42df34e01574f75c5f779f1238a4aeb8
epa <e.papet@dev1-0.com>
2015-03-06 18:34:53 +0100

    Enable OTF² CLUSTER

---
+++ otf/lib/otf_cluster.js
@@ -0,0 +1,45 @@
+/**
+ * Created by epa on 06/03/15.
+ */
+var cluster = require("cluster");
+var log4js = require('log4js');
+var logger = log4js.getLogger('www');
+var loggerMongo = log4js.getLogger('mongo');
+
+if (cluster.isMaster) {
+    var numCPUs;
+    if (GLOBAL.config["CLUSTER"].nb_cpu == 'undefine')
+        numCPUs = require('os').cpus().length;
+    else
+        numCPUs = GLOBAL.config["CLUSTER"].nb_cpu;
+    //
+    logger.info("OTF² NB_CPU [%s]", numCPUs);
+    // Fork workers.
+    for (var i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+    //
+    cluster.on('exit', function (worker, code, signal) {
+        logger.INFO('worker ' + worker.process.pid + ' died');
+    });
+} else {
+    run();
+}
+
+/**
+ * START HTTP LISTENER
+ */
+function run() {
+    var app = require('../../app');
+    app.set('port', GLOBAL.config["LOGS"].port || process.env.NODE_PORT || 3000);
+    app.set('host', GLOBAL.config["LOGS"].host || process.env.NODE_HOST || "0.0.0.0");
+//
+    var server = app.listen(app.get('port'), app.get('host'), function () {
+        logger.debug("OTF² server listening on http://%s:%d", app.get('host'), app.get('port'));
+        //loggerMongo.debug("OTF² server listening on http://%s:%d", app.get('host'), app.get('port'));
+    });
+// SERVER SOCKET
+    sio.listen(server, {log: GLOBAL.config["WEBSOCK"].log});
+    logger.debug("OTF² WebSocket Started");
+//-- Middleware function
+}
