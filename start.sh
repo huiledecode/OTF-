5a05a079c325ff110a6d60a9275d3932c527afe2
stephane <stephane@mascaron.net>
2015-07-15 16:10:10 +0200

    change session mode into start.sh, now otf used Memory for developpers this is more easy to configure.

--- start.sh
+++ start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-export NODE_SESSION="REDIS"
+#export NODE_SESSION="REDIS"
 echo server OTF is starting ...
 sudo killall mongod
+#sudo killall redis-server
 sudo mongod --replSet otf_demo &
+#sudo redis-server &
 #sleep 1
 #sudo service mongodb restart
 sleep 5
