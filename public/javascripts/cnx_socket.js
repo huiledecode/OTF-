b3daa11e2bc822c37b8b5e02cc5aabb6b276281b
Stéphane MASCARON <stephane@mascaron.net>
2017-05-02 17:17:05 +0200

    modify cnx_socket.js to reload page users after oplog insertion

--- public/javascripts/cnx_socket.js
+++ public/javascripts/cnx_socket.js
@@ -1,14 +1,13 @@
 // create connection
 var socket = io.connect('http://localhost:3000');
 // Attente Ack after connection
-socket.on('ack', function (data) {
+socket.on('ack', function(data) {
     console.log('WebSocket Client is connected \n Room is :' + data.sessionid + '\n User is :' + data.user);
 });
 // Opolog event
-socket.on('insertOk', function (data) {
+socket.on('insertOk', function(data) {
     console.log('données insérées en provenance du serveur : ', data);
-    delete data.o.__proto;
-    delete data.o.__v;
+    window.location.reload(true);
     var objectStruct = Object.getOwnPropertyNames(data.o).sort();
     console.log('objectStruct : ', objectStruct);
     var tabDataToAdd = [];
@@ -18,12 +17,23 @@ socket.on('insertOk', function (data) {
     $('#listereponses').dataTable().fnAddData(tabDataToAdd);
 });
 
-socket.on('deleteOk', function (data) {
+socket.on('deleteOk', function(data) {
     console.log('données supprimées en provenance du serveur : ', data);
     var table = $('#listereponses').dataTable();
     table.fnDeleteRow($('#' + data.o._id));
     table.draw(false);
 });
 /**
- * Created by epa on 12/09/14.
+ * Created by sma on 12/09/14.
  */
+
+/**
+  <th mData="nom">Name</th>
+  <th mData="prenom">Firstname</th>
+  <th mData="login">Login</th>
+  <th mData="password">Password</th>
+  <th mData="role.code">Code</th>
+  <th mData="role.libelle">Libellé</th>
+  <th mData="telephone">Phone</th>
+  <th mData="email">Email</th>
+*/
