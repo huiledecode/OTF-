347402eb442fb0717fd8412521261c69d667aa40
Stéphane MASCARON <stephane@mascaron.net>
2018-01-09 15:06:59 +0100

    indentation

--- otf_core/lib/otf_session.js
+++ otf_core/lib/otf_session.js
@@ -6,12 +6,15 @@ var util = require("util");
 var logger = require('log4js').getLogger('otf_session');
 logger.setLevel(GLOBAL.config["LOGS"].level);
 
-module.exports = function (app, secret, cookie_name) {
+module.exports = function(app, secret, cookie_name) {
     var cookieParser = require('cookie-parser');
     var session = require("express-session");
     var sessionStore;
     // TimeOut de session en secondes et prefix de la keyy pour REDIS
-    var options = {"ttl": GLOBAL.config["SESSION"].ttl, "prefix": GLOBAL.config["SESSION"].prefix};
+    var options = {
+        "ttl": GLOBAL.config["SESSION"].ttl,
+        "prefix": GLOBAL.config["SESSION"].prefix
+    };
     var node_session = process.env.NODE_SESSION || GLOBAL.config["SESSION"].store || "MEMORY";
     // Store Memory or Redis
     if (node_session === "REDIS") {
@@ -25,7 +28,7 @@ module.exports = function (app, secret, cookie_name) {
     }
     // -- Cookies Parser
     app.use(cookieParser(secret));
-//--
+    //--
     app.use(session({
         'name': cookie_name,
         'secret': secret,
